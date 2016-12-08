4af404329af023dea4658891d69d6f2c93924dee
angelomatni2018 <angelomatni2018@u.northwestern.edu>
2016-12-07 23:43:39 -0600

    Test model

--- AllArmiesAlive.m
+++ AllArmiesAlive.m
@@ -1,6 +1,6 @@
 function out = AllArmiesAlive(armies)
     for ii = 1:size(armies,3)
-        if (sum(sum(armies)) == 0)
+        if (sum(sum(armies(:,:,ii))) == 0)
             out = false;
             return;
         end
